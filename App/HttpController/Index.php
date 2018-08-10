8e6457b89c2c78518fceb874e15577452aae37c1
liuhongxu <hlsw1994@qq.com>
2018-08-10 23:35:54 +0800

    hello

--- App/HttpController/Index.php
+++ App/HttpController/Index.php
@@ -16,6 +16,7 @@ class Index extends Controller
      */
     function index()
     {
+	$this->response()->write('hello');
         $this->response()->withHeader('Content-type', 'text/html;charset=utf-8');
         $this->response()->write('<div style="text-align: center;margin-top: 30px"><h2>欢迎使用EASYSWOOLE</h2></div></br>');
         $this->response()->write('<div style="text-align: center">您现在看到的页面是默认的 Index 控制器的输出</div></br>');
